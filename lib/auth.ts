15549d02493796666ed73ceafe78d1ff0d2abaca
Millimeter <akumakumu@gmail.com>
2025-12-16 06:16:34 +0700

    feat: verbose better-auth server

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,12 +4,22 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { nextCookies } from "better-auth/next-js";
 import { db } from "@/db";
 
+const baseURL = process.env.BETTER_AUTH_URL;
+if (!baseURL) throw new Error("BETTER_AUTH_URL is not set");
+
+const secret = process.env.BETTER_AUTH_SECRET;
+if (!secret) throw new Error("BETTER_AUTH_SECRET is not set");
+
 export const auth = betterAuth({
+	baseURL,
+	trustedOrigins: [baseURL, "https://wakimae.com"],
+	secret,
 	database: drizzleAdapter(db, {
 		provider: "pg",
 	}),
 	emailAndPassword: {
 		enabled: true,
+		disableSignUp: true,
 	},
 	plugins: [nextCookies(), username(), admin({ adminRoles: ["admin"] })],
 });
